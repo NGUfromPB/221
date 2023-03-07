3ed64b44f8c2181e8a0406654770792aefe0ce3a
NGUfromPB <99163430+NGUfromPB@users.noreply.github.com>
2023-03-07 19:33:55 +0300

    Update MainApp.java

--- src/main/java/hiber/MainApp.java
+++ src/main/java/hiber/MainApp.java
@@ -17,26 +17,30 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      User user1 = new User("user1", "lastname1", "user1@mail.ru");
-      User user2 = new User("user2", "lastname2", "user2@mail.ru");
-      User user3 = new User("user3", "lastname3", "user3@mail.ru");
-      User user4 = new User("user4", "lastname4", "user4@mail.ru");
-
       Car car1 = new Car("car1", 6);
       Car car2 = new Car("car2", 2);
       Car car3 = new Car("Car3", 3);
       Car car4 = new Car("Car4", 4);
 
+      User user1 = new User("user1", "lastname1", "user1@mail.ru",car1);
+      User user2 = new User("user2", "lastname2", "user2@mail.ru",car2);
+      User user3 = new User("user3", "lastname3", "user3@mail.ru",car3);
+      User user4 = new User("user4", "lastname4", "user4@mail.ru",car4);
 
-      userService.add(user1.setCar(car1).setUser(user1));
-      userService.add(user2.setCar(car2).setUser(user2));
-      userService.add(user3.setCar(car3).setUser(user3));
-      userService.add(user4.setCar(car4).setUser(user4));
+      car1.setUserbyId(user1);
+      car2.setUserbyId(user2);
+      car3.setUserbyId(user3);
+      car4.setUserbyId(user4);
+
+      userService.add(user1);
+      userService.add(user2);
+      userService.add(user3);
+      userService.add(user4);
 
       List<User> users = userService.listUsers();
       for (User user : users) {
-         System.out.println("First Name = "+user.getFirstName());
-         System.out.println("Car = "+user.getCar());
+         System.out.println(user.toString());
+
       }
       System.out.println(userService.getUserCar("car1", 1));
       context.close();
