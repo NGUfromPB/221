b361e4fc44d26e1fa0934d9cf468c6693cb3d7db
NGUfromPB <99163430+NGUfromPB@users.noreply.github.com>
2023-03-12 19:36:37 +0300

    Update MainApp.java

--- src/main/java/hiber/MainApp.java
+++ src/main/java/hiber/MainApp.java
@@ -17,20 +17,12 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      Car car1 = new Car("car1", 6);
-      Car car2 = new Car("car2", 2);
-      Car car3 = new Car("Car3", 3);
-      Car car4 = new Car("Car4", 4);
 
-      User user1 = new User("user1", "lastname1", "user1@mail.ru",car1);
-      User user2 = new User("user2", "lastname2", "user2@mail.ru",car2);
-      User user3 = new User("user3", "lastname3", "user3@mail.ru",car3);
-      User user4 = new User("user4", "lastname4", "user4@mail.ru",car4);
 
-      car1.setUserbyId(user1);
-      car2.setUserbyId(user2);
-      car3.setUserbyId(user3);
-      car4.setUserbyId(user4);
+      User user1 = new User("user1", "lastname1", "user1@mail.ru",new Car("mazda",1));
+      User user2 = new User("user2", "lastname2", "user2@mail.ru",new Car("lada",2));
+      User user3 = new User("user3", "lastname3", "user3@mail.ru",new Car("kia",3));
+      User user4 = new User("user4", "lastname4", "user4@mail.ru",new Car("mers",4));
 
       userService.add(user1);
       userService.add(user2);
@@ -39,7 +31,7 @@ public class MainApp {
 
       List<User> users = userService.listUsers();
       System.out.println(users);
-      System.out.println(userService.getUserCar("car1", 1));
+      System.out.println("User with car" + userService.getUserCar("mazda", 1));
       context.close();
    }
 }
