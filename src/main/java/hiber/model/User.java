5802555ea6ee4eed07238ee9621b754cc56b7a91
NGUfromPB <99163430+NGUfromPB@users.noreply.github.com>
2023-03-12 19:37:21 +0300

    Update User.java

--- src/main/java/hiber/model/User.java
+++ src/main/java/hiber/model/User.java
@@ -18,24 +18,59 @@ public class User {
 
    @Column(name = "email")
    private String email;
-   @OneToOne (cascade = CascadeType.ALL, mappedBy = "user")
+
+   @OneToOne(cascade = CascadeType.ALL)
+   @JoinColumn(name = "car_id")
    private Car car;
 
-   public Car getCar() {
-      return car;
+   public User() {
+   }
+
+   public User(String firstName, String lastName, String email, Car car) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.email = email;
+      this.car = car;
+   }
+
+   public Long getId() {
+      return id;
+   }
+
+   public void setId(Long id) {
+      this.id = id;
    }
 
    public String getFirstName() {
       return firstName;
    }
 
-   public User() {}
-
-   public User(String firstName, String lastName, String email,Car car) {
+   public void setFirstName(String firstName) {
       this.firstName = firstName;
+   }
+
+   public String getLastName() {
+      return lastName;
+   }
+
+   public void setLastName(String lastName) {
       this.lastName = lastName;
+   }
+
+   public String getEmail() {
+      return email;
+   }
+
+   public void setEmail(String email) {
       this.email = email;
-      this.car=car;
+   }
+
+   public Car getCar() {
+      return car;
+   }
+
+   public void setCar(Car car) {
+      this.car = car;
    }
 
    @Override
